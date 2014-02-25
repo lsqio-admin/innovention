8bdb16420f8472eb034bdea2a605d7ccf8c76287
Antonio pelle Pellegrino <pelle@circles.io>
2014-02-24 20:24:54 -0500

    Update

--- org/js/views/index.js
+++ org/js/views/index.js
@@ -14,7 +14,25 @@
             },
             render:function(){
                 var that       = this;
-                that.$el.html(this.home({}))
+                if(_.isObject(that.___.p)){
+                    that.$el.html(this.home({
+                         name: that.___.p.get("title")
+                        ,log: that.___.u.loggedIn
+                        ,email: (that.___.p.get("email").indexOf("@nyu") != -1)
+                    }))
+                }else{
+                    that.$el.html(this.home({
+                         name: ""
+                        ,log: that.___.u.loggedIn
+                        ,email: false
+                    }))
+                }
+                
+
+                    
+                
+
+
                /* that.items.fetch({
                     success:function(){
                     },data:{}
